d43bff18a9574ba9ad577b44a0986722f6d33dac
prince-sunsara <princesunasara977@gmail.com>
2023-08-16 10:33:17 +0530

    initial Commit

---
+++ javascript/modules.js
@@ -0,0 +1,8 @@
+/* in-line export : */
+// export const name = "Jesse";
+// export const age = 21;
+
+/* All at once at the bottom : */
+const name = "Jesse";
+const age = 21;
+export { name, age };
---
+++ javascript/modules2.js
@@ -0,0 +1,7 @@
+// Default Export : You can only have one default export in a file.
+const message = () => {
+    const name = "Jesse";
+    const age = 21;
+    return name + ' is ' + age + 'years old.';
+};
+export default message;
